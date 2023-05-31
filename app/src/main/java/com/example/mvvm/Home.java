986a15215da1e176d2197652aa96a114096a54fc
VinhNQ <vinhnq@irismedia.vn>
2023-05-31 10:55:18 +0700

    add navigation

--- app/src/main/java/com/example/mvvm/Home.java
+++ app/src/main/java/com/example/mvvm/Home.java
@@ -1,14 +1,47 @@
 package com.example.mvvm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class Home extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_search:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.navigation_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectedFragment).commit();
+            return true;
+        });
+        // Mặc định hiển thị Home Fragment khi ứng dụng khởi động
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new HomeFragment()).commit();
     }
 }
